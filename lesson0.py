e9d98a70c9e9134e4ebc23e37b6ecfd458c4d39c
SunnyHoneyFox <smv1106@gmail.com>
2024-06-15 18:27:09 +0300

    Add files via upload

---
+++ lesson0.py
@@ -0,0 +1,25 @@
+a = 'To be or not to be?'
+print(len(a))
+
+first = 11
+second = 6
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+
+first = 8
+second = 3
+third = 5
+mean = (first + second + third) / 3
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string+',',first_string)
+
+a = 9
+b = 2
+c = 4
+f = (a * b) + (a * c)
+print(f ** 3 / 2)
