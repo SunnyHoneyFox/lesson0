c7af92005e4bc1f185b620fbdec52c01358bc712
Marina <smv1106@gmail.com>
2024-06-20 20:25:25 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+got_it = 12
+time = 1.50
+course = 'Python'
+time_for_one = time/got_it
+print('Курс:',course+','' всего задач:',str(got_it)+', затрачено часов:',str(time)+', среднее время выполнения',time_for_one,'часа.')
