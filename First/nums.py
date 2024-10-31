cc295dd1dc43b0fe6dfd80fb068a3970cc300f29
SunnyHoneyFox <smv1106@gmail.com>
2024-10-31 23:49:16 +0300

    добавила функцию divide

--- First/nums.py
+++ First/nums.py
@@ -1,13 +1,22 @@
-def plus(a,b):
-    return a+b
+def plus(a, b):
+    return a + b
 
-def minus(a,b):
-    return a-b
 
-def multiply(a,b):
-    return a*b
+def minus(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
 
 if __name__ == '__main__':
-    print(plus(5,2))
-    print(minus(5,2))
-    print(multiply(5,2))
+    print(plus(5, 2))
+    print(minus(5, 2))
+    print(multiply(5, 2))
+    print(divide(5, 2))
