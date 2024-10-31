e18a9b7dc96e07a17843a15081a8335f7ea061e1
SunnyHoneyFox <smv1106@gmail.com>
2024-10-31 23:43:10 +0300

    добавила функцию multiplay

--- First/nums.py
+++ First/nums.py
@@ -4,6 +4,10 @@ def plus(a,b):
 def minus(a,b):
     return a-b
 
+def multiply(a,b):
+    return a*b
+
 if __name__ == '__main__':
     print(plus(5,2))
     print(minus(5,2))
+    print(multiply(5,2))
